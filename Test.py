d73a69b1b3a72c1fcf69c0547bdc685d6e670341
Claire Robin <clairerobin88@gmail.com>
2025-03-21 12:05:25 +0100

    éviter un merge conflict v2

--- Test.py
+++ Test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Questions imposées :
 
 # – Déterminez le nombre de médailles gagnées par Michael Phelps. Son nom complet est
@@ -7,14 +6,13 @@
 # pour les Jeux Olympiques de 2016.
 
 # id de Michael Phelps : 94406.
-||||||| a40026f
+
 # lalalala
 # test2
 # lalalav2
-=======
+
 from lecture_donnees import donnees_athlete_events
 from lecture_donnees import donnees_noc_regions
 
 donnees_athlete_events
 donnees_noc_regions
->>>>>>> bdbbb8037237a209a125a56c601d6bb5e548abfb
--- lecture_donnees.py
+++ lecture_donnees.py
@@ -10,11 +10,11 @@ donnees_noc_regions = []
 with open(chemin_csv1, 'r', encoding='utf-8') as fichier:
     for ligne in fichier:
         ligne = ligne.strip()
-        elements = ligne.split(',') 
+        elements = ligne.split(',')
         donnees_athlete_events.append(elements)
 
 with open(chemin_csv2, 'r', encoding='utf-8') as fichier:
     for ligne in fichier:
         ligne = ligne.strip()
-        elements = ligne.split(',') 
+        elements = ligne.split(',')
         donnees_noc_regions.append(elements)
