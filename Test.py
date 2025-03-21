61a99eb161e159170d7bb034dd6e8a8ccc2f1532
AntoineR18 <rustenholz.antoine18@gmail.com>
2025-03-21 12:14:47 +0100

    test passe Flake8

--- Test.py
+++ Test.py
@@ -1,9 +1,9 @@
 # Questions imposées :
 
-# – Déterminez le nombre de médailles gagnées par Michael Phelps. Son nom complet est
-# Michael Fred Phelps, II.
-# – Trouvez des bornes inférieures et supérieures pour le nombre de médailles par nation
-# pour les Jeux Olympiques de 2016.
+# – Déterminez le nombre de médailles gagnées par Michael Phelps. Son nom
+# complet est Michael Fred Phelps, II.
+# – Trouvez des bornes inférieures et supérieures pour le nombre de médailles
+# par nation pour les Jeux Olympiques de 2016.
 
 # id de Michael Phelps : 94406.
 
--- lecture_donnees.py
+++ lecture_donnees.py
@@ -7,14 +7,14 @@ donnees_athlete_events = []
 donnees_noc_regions = []
 
 
-with open(chemin_csv1, 'r', encoding='utf-8') as fichier:
+with open(chemin_csv1, "r", encoding="utf-8") as fichier:
     for ligne in fichier:
         ligne = ligne.strip()
-        elements = ligne.split(',')
+        elements = ligne.split(",")
         donnees_athlete_events.append(elements)
 
-with open(chemin_csv2, 'r', encoding='utf-8') as fichier:
+with open(chemin_csv2, "r", encoding="utf-8") as fichier:
     for ligne in fichier:
         ligne = ligne.strip()
-        elements = ligne.split(',')
+        elements = ligne.split(",")
         donnees_noc_regions.append(elements)
