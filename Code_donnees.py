5cc4bd618c4aa6a9ce9b87930c85759e9e1d974c
Claire Robin <clairerobin88@gmail.com>
2025-03-21 11:57:36 +0100

    éviter un merge conflict

--- Code_donnees.py
+++ Code_donnees.py
@@ -8,9 +8,14 @@ donnees = []
 with open(chemin_csv, 'r', encoding='utf-8') as fichier:
     for ligne in fichier:
         ligne = ligne.strip()
-        elements = ligne.split(',') 
+        elements = ligne.split(',')
         donnees.append(elements)
 
 # Afficher les 5 premières lignes
 for ligne in donnees[:5]:
     print(ligne)
+
+for elements in donnees:
+    nombre_medailles = 0
+    if elements[-1] != 'NA':
+        nombre_medailles
--- Test.py
+++ Test.py
@@ -1,3 +1,8 @@
-# lalalala
-# test2
-# lalalav2
+# Questions imposées :
+
+# – Déterminez le nombre de médailles gagnées par Michael Phelps. Son nom complet est
+# Michael Fred Phelps, II.
+# – Trouvez des bornes inférieures et supérieures pour le nombre de médailles par nation
+# pour les Jeux Olympiques de 2016.
+
+# id de Michael Phelps : 94406.
