f98b5a649db2c31dac3865a5643ea4d31d955613
AntoineR18 <rustenholz.antoine18@gmail.com>
2025-04-11 12:49:54 +0200

    Q5 python et Q5 panda

---
+++ Question5_panda.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+# Importation et sélection des données utiles
+
+dta = pd.read_csv("donnees_jeux_olympiques/athlete_events.csv")
+dta_utile = dta[["ID", "NOC", "Year", "Medal"]]
+
+
+# Pays non médaillé le plus représenté sur une année donnée.
+
+
+def pays_non_medailles_max_annee(annee):
+    dico = {}
+    dta_annee = dta_utile[dta_utile["Year"] == annee]
+    for noc in dta_annee["NOC"].unique():
+        dta_pays = dta_annee[dta_annee["NOC"] == noc]
+        if not dta_pays["Medal"].isna().all():
+            continue
+        dico[noc] = dta_pays["ID"].nunique()
+    return f"En {annee}, le pays non médaillé le plus représenté était : {max(dico, key=dico.get)}."
+
+
+# print(pays_non_medailles_max_annee(2016))
+
+# Pays non médaillé le plus représenté dans l'histoire.
+
+
+def pays_non_medailles_max_histoire():
+    dico = {}
+    for noc in dta_utile["NOC"].unique():
+        dta_pays = dta_utile[dta_utile["NOC"] == noc]
+        if not dta_pays["Medal"].isna().all():
+            continue
+    return dico
+
+
+print(pays_non_medailles_max_histoire())
