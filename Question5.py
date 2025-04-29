17a59954ad8ef63d3b68c27c0f7466e9277ae5df
AntoineR18 <rustenholz.antoine18@gmail.com>
2025-04-29 12:51:37 +0200

    Q5 Panda finie, Q5 python en cours, Q7 presque finie

--- Question5.py
+++ Question5.py
@@ -10,18 +10,23 @@ donnees = donnees_athlete_events[1:]
 
 idx_noc = entete.index("NOC")
 idx_annee = entete.index("Year")
-idx_athlete = entete.index("Name")
+idx_athlete = entete.index("ID")
 idx_sport = entete.index("Sport")
 
 
 # Pays non médaillé le plus réprésenté sur une année donnée
 
+
 def pays_non_medaille_max_annee(annee, sortie):
     pays_medaille = {ligne[0]: False for ligne in donnees_noc_regions}
     dico = {}
+    athletes = [False for i in range(0, int(donnees[-1][0]) + 1)]
     for ligne in donnees:
         if ligne[idx_annee] != annee:
             continue
+        if athletes[int(ligne[idx_athlete])]:
+            continue
+        athletes[int(ligne[idx_athlete])] = True
         noc = ligne[idx_noc]
         if noc == "SGP":
             noc = "SIN"
@@ -34,57 +39,93 @@ def pays_non_medaille_max_annee(annee, sortie):
         if noc not in dico:
             dico[noc] = 0
         dico[noc] += 1
+    pays = max(dico, key=dico.get)
     if sortie == "dico":
         return dico
     elif sortie == "pays":
-        return (f"En {annee}, le pays non médaillé le plus représenté était :"
-                f" {max(dico, key=dico.get)}.")
+        return (
+            f"En {annee}, le pays non médaillé le plus représenté était"
+            f" {pays} avec {dico[pays]} participants."
+        )
 
-# print(pays_non_medaille_max_annee("2016", "pays"))
+
+# print(pays_non_medaille_max_annee("1912", "dico"))
 
 
 # Liste des années vérifiée
 
-annees = [annee for annee in range(1896, 1993, 4)]
-annees.pop(annees.index(1916))
-annees.pop(annees.index(1940))
-annees.pop(annees.index(1944))
-annees.append(1906)
-annees.extend([annee for annee in range(1994, 2017, 2)])
-annees.sort()
 
+def liste_annees():
+    liste = [annee for annee in range(1896, 1993, 4)]
+    liste.pop(liste.index(1916))
+    liste.pop(liste.index(1940))
+    liste.pop(liste.index(1944))
+    liste.append(1906)
+    liste.extend([annee for annee in range(1994, 2017, 2)])
+    liste.sort()
+    return liste
+
+
+annees = liste_annees()
 
 # Dictionnaire des pays par année
 
-noc_par_annee = {annee: [] for annee in annees}
-for noc_annee in pays_final:
-    i = len(annees) - 1
-    while i >= 0 and annees[i] >= int(noc_annee[1]):
-        noc_par_annee[annees[i]].append(noc_annee[0])
-        i -= 1
-print(noc_par_annee)
 
+def dico_noc_par_annee():
+    dico = {annee: [] for annee in annees}
+    for noc_annee in pays_final:
+        i = len(annees) - 1
+        while i >= 0 and annees[i] >= int(noc_annee[1]):
+            dico[annees[i]].append(noc_annee[0])
+            i -= 1
+    return dico
+
+
+noc_par_annee = dico_noc_par_annee()
 
 
 # Pays non médaillé le plus représenté dans l'histoire
 
 
-# def pays_non_medaille_plus_participants_toujours():
-#     pays_non_medailles_toujours = pays_non_medaille_max_annee(
-#         str(annees[0]),
-#         "dico"
-#     )
-#     for annee in annees[1:]:
-#         print(annee)
-#         pays_non_medailles_annee = pays_non_medaille_max_annee(
-#             str(annee)
-#         )
-#         for noc in pays_non_medailles_toujours.keys():
-#             if noc not in pays_non_medailles_annee:
-#                 del pays_non_medailles_toujours[noc]
-#                 continue
-#             pays_non_medailles_toujours[noc] += pays_non_medailles_annee[noc]
-#     return pays_non_medailles_toujours
+def pays_non_medaille_max_histoire(sortie):
+    dico_statut = {
+        ligne[0]: {"participe": False, "medaille": False}
+        for ligne in donnees_noc_regions
+    }
+    dico_pays = pays_non_medaille_max_annee(str(annees[0]), "dico")
+    for noc in noc_par_annee[annees[0]]:
+        dico_statut[noc]["participe"] = True
+        if noc not in dico_pays:
+            dico_statut[noc]["medaille"] = True
+    i = 1
+    while i < len(annees):
+        annee = annees[i]
+        dico_annee = pays_non_medaille_max_annee(str(annee), "dico")
+        for noc in noc_par_annee[annee]:
+            if dico_statut[noc]["participe"] and dico_statut[noc]["medaille"]:
+                if noc in dico_annee:
+                    del dico_annee[noc]
+                continue
+            if not dico_statut[noc]["participe"]:
+                dico_statut[noc]["participe"] = True
+                if noc not in dico_annee:
+                    dico_statut[noc]["medaille"] = True
+        for noc in dico_annee:
+            if noc in dico_pays:
+                dico_pays[noc] += dico_annee[noc]
+            else:
+                dico_pays[noc] = dico_annee[noc]
+        i += 1
+    pays = max(dico_pays, key=dico_pays.get)
+    if sortie == "dico statut":
+        return dico_statut
+    elif sortie == "dico pays":
+        return dico_pays
+    elif sortie == "pays":
+        return (
+            f"Dans toute l'histoire, le pays non médaillé le plus représenté"
+            f" est {pays} avec {dico_pays[pays]} participants."
+        )
 
 
-# print(pays_non_medaille_plus_participants_toujours())
+print(pays_non_medaille_max_histoire("pays"))
